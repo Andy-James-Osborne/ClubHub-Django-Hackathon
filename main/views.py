f2e88c52ee9c8c2cbe776c790a4681582f201cef
Tom Cowen <tomreescowen@gmail.com>
2024-03-06 13:12:07 +0000

    Add meta data to index.html and add style.css

--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+    
+def event_details(request):
+    return render(request, 'events.html')
+
+def logout_user(request):
+    return render(request, 'logout.html')
