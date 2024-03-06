914f94e23b6cd6da6e11f60b92710abef941a4d9
Andy Osborne <andyjamesosborne@proton.me>
2024-03-06 10:30:39 +0000

    Added an Events section and a comment section to the models to implement to the events page for the models section

--- main/models.py
+++ main/models.py
@@ -7,3 +7,23 @@ class User(models.Model):
     
     def __str__(self):
         return self.username
+
+class Events(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    image = models.TextField(upload_to='event-image')
+    status = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Events, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
