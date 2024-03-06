d8ad6960248a812302c161bde14ed54a53db51f4
Andy Osborne <andyjamesosborne@proton.me>
2024-03-06 10:47:50 +0000

    Create views for login and logout user with a register function if not a user

--- main/views.py
+++ main/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, Events, Comments
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    context = {'form' : form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    else: 
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect(request, 'login.html')
