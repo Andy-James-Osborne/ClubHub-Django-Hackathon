82853abc88dbcb4f4b452b53d4eb9918551d8f8c
Tom Cowen <tomreescowen@gmail.com>
2024-03-06 12:13:38 +0000

    Add html files

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_user, name='login'),
     path('register/', views.register, name='register'),
-    path('events/', views.events, name='events'),
+    path('events/', views.event_details, name='event_details'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,12 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
-
-def login_user(request):
-    return render(request, 'login.html')
-
-def register(request):
-    return render(request, 'register.html')
-    
-def events(request):
-    return render(request, 'events.html')
