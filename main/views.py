59cedbce45d230c854fef263f081b6877bca58ae
Tom Cowen <tomreescowen@gmail.com>
2024-03-06 11:34:09 +0000

    Create html files in templates and add to views

--- main/views.py
+++ main/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+    
+def events(request):
+    return render(request, 'events.html')
