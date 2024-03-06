066fd3bfc99979d1cb0ab2b0ebd880632022b2cd
Andy Osborne <andyjamesosborne@proton.me>
2024-03-06 11:23:20 +0000

    Added the models to the admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Events, Comment, User
 
 # Register your models here.
+admin.site.register(Events)
+admin.site.register(Comment)
+admin.site.register(User)
